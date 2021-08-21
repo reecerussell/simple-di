df31639d9ac7dd40db1fda813f85a425f15fc574
Reece Russell <me@reece-russell.co.uk>
2021-08-21 17:39:27 +0100

    Added DI container

---
+++ di/conatiner.go
@@ -0,0 +1,62 @@
+package di
+
+// Container is a simple dependency injection container.
+type Container struct {
+	srvs    map[string]interface{}
+	srvConf map[string]*ServiceConfig
+}
+
+// NewContainer returns a new Container.
+func NewContainer() *Container {
+	return &Container{
+		srvs:    make(map[string]interface{}),
+		srvConf: make(map[string]*ServiceConfig),
+	}
+}
+
+// BuildFunc is a function used to build a service.
+type BuildFunc func(ctn *Container) interface{}
+
+// ServiceConfig represents a service within the Container.
+type ServiceConfig struct {
+	Singleton bool
+	Build     BuildFunc
+}
+
+// GetService attempts to resolve a service by name.
+func (ctn *Container) GetService(name string) interface{} {
+	conf, ok := ctn.srvConf[name]
+	if !ok {
+		panic("unable to resolve service: " + name)
+	}
+
+	srv, ok := ctn.srvs[name]
+	if conf.Singleton && ok {
+		return srv
+	}
+
+	impl := conf.Build(ctn)
+
+	if conf.Singleton {
+		ctn.srvs[name] = impl
+	}
+
+	return impl
+}
+
+// AddService adds a new service definition to the container.
+func (ctn *Container) AddService(name string, builder BuildFunc) {
+	ctn.addService(name, false, builder)
+}
+
+// AddSingleton adds a new singleton service definition to the container.
+func (ctn *Container) AddSingleton(name string, builder BuildFunc) {
+	ctn.addService(name, true, builder)
+}
+
+func (ctn *Container) addService(name string, singleton bool, builder BuildFunc) {
+	ctn.srvConf[name] = &ServiceConfig{
+		Singleton: singleton,
+		Build:     builder,
+	}
+}
---
+++ di/container_test.go
@@ -0,0 +1,94 @@
+package di
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestContainer_GetNonExistantService_Panics(t *testing.T) {
+	ctn := &Container{
+		srvs:    make(map[string]interface{}),
+		srvConf: make(map[string]*ServiceConfig),
+	}
+
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("Expected panic")
+		}
+	}()
+
+	_ = ctn.GetService("myService")
+}
+
+func TestContainer_AddService(t *testing.T) {
+	builder := func(ctn *Container) interface{} {
+		return nil
+	}
+	name := "myService"
+
+	ctn := NewContainer()
+	ctn.AddService(name, builder)
+
+	srv, ok := ctn.srvConf[name]
+	assert.True(t, ok)
+	assert.False(t, srv.Singleton)
+}
+
+func TestContainer_AddSingleton(t *testing.T) {
+	builder := func(ctn *Container) interface{} {
+		return nil
+	}
+	name := "myService"
+
+	ctn := NewContainer()
+	ctn.AddSingleton(name, builder)
+
+	srv, ok := ctn.srvConf[name]
+	assert.True(t, ok)
+	assert.True(t, srv.Singleton)
+}
+
+func TestContainer_SingletonNotRecreated(t *testing.T) {
+	ctn := &Container{
+		srvs:    make(map[string]interface{}),
+		srvConf: make(map[string]*ServiceConfig),
+	}
+
+	ctn.srvConf["test"] = &ServiceConfig{
+		Singleton: true,
+		Build: func(ctn *Container) interface{} {
+			srvValue := "My super cool service"
+			return &srvValue
+		},
+	}
+
+	srv := ctn.GetService("test")
+	srv2 := ctn.GetService("test")
+
+	if srv != srv2 {
+		t.Error("Expected the services to be equal")
+	}
+}
+
+func TestContainer_TransientIsRecreated(t *testing.T) {
+	ctn := &Container{
+		srvs:    make(map[string]interface{}),
+		srvConf: make(map[string]*ServiceConfig),
+	}
+
+	ctn.srvConf["test"] = &ServiceConfig{
+		Singleton: false,
+		Build: func(ctn *Container) interface{} {
+			srvValue := "My super cool service"
+			return &srvValue
+		},
+	}
+
+	srv := ctn.GetService("test")
+	srv2 := ctn.GetService("test")
+
+	if srv == srv2 {
+		t.Error("Expected the services to not be equal")
+	}
+}
