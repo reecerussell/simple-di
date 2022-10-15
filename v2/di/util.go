de5b06009e8c0637677b77aceee85313e5d96cf0
Reece Russell <reece.russell@thebodyshop.com>
2022-10-15 15:44:02 +0100

    fix: corrected type naming in utils

--- v2/di/util.go
+++ v2/di/util.go
@@ -3,8 +3,11 @@ package di
 import "reflect"
 
 func GetService[T any](sp ServiceProvider) T {
-	t := reflect.TypeOf(*new(T))
-	return GetServiceByName[T](sp, t.Name())
+	t := reflect.TypeOf(new(T))
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+	return GetServiceByName[T](sp, t.String())
 }
 
 func GetServiceByName[T any](sp ServiceProvider, name string) T {
