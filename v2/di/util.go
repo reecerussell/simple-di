818223309fa279769ba34b6a463380d2a797d4e0
Reece Russell <reece.russell@thebodyshop.com>
2022-10-19 18:06:42 +0100

    fix: corrected the name logic in utils

--- v2/di/util.go
+++ v2/di/util.go
@@ -2,14 +2,21 @@ package di
 
 import "reflect"
 
+// GetService is generic function used to get a service
+// from the given ServiceProvider.
 func GetService[T any](sp ServiceProvider) T {
 	t := reflect.TypeOf(new(T))
 	if t.Kind() == reflect.Ptr {
 		t = t.Elem()
+		if t.Kind() == reflect.Ptr {
+			t = t.Elem()
+		}
 	}
 	return GetServiceByName[T](sp, t.String())
 }
 
+// GetService is generic function used to get a service
+// from the given ServiceProvider, using the specifed name.
 func GetServiceByName[T any](sp ServiceProvider, name string) T {
 	return sp.GetService(name).(T)
 }
--- v2/di/util_test.go
+++ v2/di/util_test.go
@@ -18,6 +18,18 @@ func TestGetService_GivenType_ReturnsService(t *testing.T) {
 	assert.NotNil(t, v)
 }
 
+func TestGetService_GivenPointerType_ReturnsService(t *testing.T) {
+	ctor := func() *testDependency {
+		return &testDependency{}
+	}
+
+	ctn := NewContainer()
+	ctn.AddService(ctor)
+
+	v := GetService[*testDependency](ctn)
+	assert.NotNil(t, v)
+}
+
 func TestGetService_GivenInterfaceType_ReturnsService(t *testing.T) {
 	ctor := func() TestService {
 		return testService{}
